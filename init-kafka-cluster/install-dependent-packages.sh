5ab55f845451d816206eabdb96f73f3249b93f6b
Evgeniy Popov <mr.demix@yandex.ru>
2026-01-20 11:27:25 +0000

    fix typos

---
+++ init-kafka-cluster/install-dependent-packages.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install docker.io \
+                 docker-compose-v2
+                 python3.12-venv
+
+python3.12 -m venv .venv
+source .venv/bin/activate
+pip install ansible
